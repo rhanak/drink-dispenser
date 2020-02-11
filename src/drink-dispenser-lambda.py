73cb73d57efe0732a64d9daacc99a8971f6b13c1
Randal Hanak <randal.hanakjr@rsa.com>
2020-02-11 16:08:43 -0500

    Add some protections around safe checking of input data

--- src/drink-dispenser-lambda.py
+++ src/drink-dispenser-lambda.py
@@ -4,6 +4,7 @@ import boto3
 import logging
 import hashlib
 import hmac
+from datetime import datetime
 
 # Create a logger
 logger = logging.getLogger(__name__)
@@ -19,35 +20,49 @@ except Exception as e:
 GITHUB_TOKEN = os.environ['GITHUB_TOKEN'].encode("utf-8")
 
 def lambda_handler(event, context):
-    logger.info(GITHUB_TOKEN)
    
-    logger.info("Event: %s" % json.dumps(event))
+    # Dump the event for debugging purposes
+    logger.debug("Event: %s" % json.dumps(event))
     
-    if 'headers' in event:
+    # Ensure some preconditions are met
+    if event.get("headers", {}).get("X-Hub-Signature"):
+        # Pull out the headers and body for later verification
         headers = event["headers"]
         payload = event["body"]
-        logger.info("Signature \"%s\"" % headers["X-Hub-Signature"])
+        logger.debug("Signature \"%s\"" % headers["X-Hub-Signature"])
         
-        # assuming that the 'payload' variable keeps the content sent by github as plain text
-        # and 'headers' variable keeps the headers sent by GitHub
+        # Check we have been provided a valid signature
+        if len(headers['X-Hub-Signature'].split('=')) == 2 and len(payload) > 10:
+            providedSignature = headers['X-Hub-Signature'].split('=')[1]
+        else:
+            return {
+                'statusCode': 403,
+                'body': "Not Authorized"
+            }
+        
+         # Using the secret key and the payload generate a signature
         signature = hmac.new(GITHUB_TOKEN, payload.encode("utf-8"), hashlib.sha1).hexdigest()
-        if hmac.compare_digest(signature, headers['X-Hub-Signature'].split('=')[1]):
+            
+        # Compare the signature we have generated matches the one on the incoming request
+        if hmac.compare_digest(signature, providedSignature):
 
             response = iotDataclient.update_thing_shadow(
                 thingName='Dispenser',
-                payload=b'{ "state": { "desired": { "welcome": "aws-iot", "led_ring": { "count": 1, "color": "#FFFFFF" }, "led": "on", "dispense_time_ms": 800, "request": { "command": "dispense" } } } }'
+                payload=b'{ "state": { "desired": { "welcome": "aws-iot", "led_ring": { "count": 1, "color": "#FFFFFF" }, "led": "on", "dispense_time_ms": 500, "request": { "command": "dispense" } } } }'
             )
             logger.info("Update shadow: %s" % str(response))
             
             return {
                 'statusCode': 200,
-                'body': json.dumps('Hello from Lambda!')
+                'body': "Updated shadow at %s." % datetime.now()
             }
         else:
             logger.info("Not authorized.")
         
     else:
         logger.info("No headers passed.")
-    
-    
-
+        
+    return {
+        'statusCode': 403,
+        'body': "Not Authorized"
+    }
